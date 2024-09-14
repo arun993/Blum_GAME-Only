281b5f9c892e5421a632f37e2b043ff66d67e53e
Shobhit-Roshan <136467972+Shobhit-Roshan@users.noreply.github.com>
2024-09-14 19:20:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,7 +107,7 @@ class Blum:
                     base.log(f"{base.red}Error: {base.white}{e}")
 
             print()
-            wait_time = 60 * 60
+            wait_time = 3 * 60 * 60
             base.log(f"{base.yellow}Wait for {int(wait_time/60)} minutes!")
             time.sleep(wait_time)
 
